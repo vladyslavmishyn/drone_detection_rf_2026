0852f74a145dc3c41042f606829b2d3b34aa0af0
Olya <volyanikolya@gmail.com>
2026-01-16 15:54:49 +0000

    map with hyperbolas

--- simulation/live_map.py
+++ simulation/live_map.py
@@ -5,7 +5,25 @@ import matplotlib.pyplot as plt
 from typing import Optional, Tuple
 import contextily as cx
 
+from tdoa_sim import all_pairs
 
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from src.core import (
+    generate_pairwise_tdoa,
+    plot_hyperbola_2d,
+    get_bounds_from_nodes,
+)
+
+"""
+run plot_map(receivers_coords) once to visualize the map
+run plot_drone((lat, lon), drone_id, show_hyperbolas) multiple times to update drone positions
+
+"""
+
+
+nodes = None
 fig = None
 ax = None
 drone_scatter = None
@@ -13,26 +31,6 @@ drone_scatter = None
 
 drones = {}
 drone_labels = {}
-
-def bounds_with_padding(xy: np.ndarray, pad_ratio: float = 0.35):
-    """
-    xy: (x, y) in meters (EPSG:3857)
-    """
-    xmin, ymin = xy.min(axis=0)
-    xmax, ymax = xy.max(axis=0)
-
-    dx = xmax - xmin
-    dy = ymax - ymin
-    if dx == 0:
-        dx = 100.0
-    if dy == 0:
-        dy = 100.0
-
-    padx = dx * pad_ratio
-    pady = dy * pad_ratio
-
-    return xmin - padx, xmax + padx, ymin - pady, ymax + pady
-
 
 def match_fig_aspect(xmin, xmax, ymin, ymax, fig_w, fig_h):
     """Expand bounds so (xrange/yrange) matches (fig_w/fig_h)"""
@@ -57,15 +55,16 @@ def match_fig_aspect(xmin, xmax, ymin, ymax, fig_w, fig_h):
     return xmin, xmax, ymin, ymax
 
 
-def plot_map(receivers_coords: np.ndarray) -> None:
+def plot_map(receivers_coords):
     """
     receivers_coords: array of (lon, lat) in degrees (EPSG:4326)
     """
 
-    global fig, ax, drone_scatter
+    global fig, ax, drone_scatter, nodes
+    nodes = np.array(receivers_coords)
 
     gdf = gpd.GeoDataFrame(
-        geometry=[Point(lon, lat) for lon, lat in receivers_coords],
+        geometry=[Point(lon, lat) for lon, lat in nodes],
         crs="EPSG:4326",
     ).to_crs(epsg=3857)
 
@@ -78,7 +77,7 @@ def plot_map(receivers_coords: np.ndarray) -> None:
         ax.text(p.x + 80, p.y + 80, f"N{k}")
 
     xy = np.array([(p.x, p.y) for p in gdf.geometry], dtype=float)
-    xmin, xmax, ymin, ymax = bounds_with_padding(xy, pad_ratio=0.35)
+    xmin, xmax, ymin, ymax = get_bounds_from_nodes(xy)
     xmin, xmax, ymin, ymax = match_fig_aspect(
         xmin, xmax, ymin, ymax,
         fig.get_figwidth(), fig.get_figheight()
@@ -94,38 +93,27 @@ def plot_map(receivers_coords: np.ndarray) -> None:
     plt.show(block=False)
 
 
-
-def plot_drone(coordinates, drone_id, hyperbolas=None):
+def plot_drone(coordinates, drone_id, show_hyperbolas=True):
     """
-    Plot the drone / update it's coordinates on the live map given its coordinates and id
-    coordinates is an array (latitude, longitude)
+    coordinates: (latitude, longitude) in degrees
     """
-    #if the map has not been initialized yet, raise an error
-    #if the drone with drone_id was already plotted, update its position
-    #if position outside of bounds, do not plot
-
     global fig, ax, drones, drone_labels
 
     if fig is None or ax is None:
-        raise RuntimeError("Call plot_map(receivers_coords) before plot_drone(...).")
+        raise RuntimeError("Call plot_map() before plot_drone(...).")
 
+    #convert drone to meters
     lat, lon = float(coordinates[0]), float(coordinates[1])
-
     g = gpd.GeoSeries([Point(lon, lat)], crs="EPSG:4326").to_crs(epsg=3857)
     x, y = float(g.iloc[0].x), float(g.iloc[0].y)
 
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
-
     inside = (xmin <= x <= xmax) and (ymin <= y <= ymax)
 
+    # create the drone if not exists
     if drone_id not in drones:
-        drones[drone_id] = ax.scatter(
-            [x], [y],
-            marker="o",
-            s=90,
-            zorder=10,
-        )
+        drones[drone_id] = ax.scatter([x], [y], marker="o", s=90, zorder=10)
         drone_labels[drone_id] = ax.text(
             x + 100, y + 100,
             str(drone_id),
@@ -133,30 +121,81 @@ def plot_drone(coordinates, drone_id, hyperbolas=None):
             fontsize=9,
             weight="bold",
         )
-        fig.canvas.draw_idle()
-        fig.canvas.flush_events()
+        drones[drone_id]._hyperbolas = []  # store artists here
 
+    # hide if outside
     if not inside:
         drones[drone_id].set_visible(False)
         drone_labels[drone_id].set_visible(False)
+
+        # remove hyperbolas
+        for art in drones[drone_id]._hyperbolas:
+            try:
+                art.remove()
+            except Exception:
+                pass
+        drones[drone_id]._hyperbolas.clear()
+
+        fig.canvas.draw_idle()
+        fig.canvas.flush_events()
         return
 
+    
+    # if inside, update the position
     drones[drone_id].set_visible(True)
     drone_labels[drone_id].set_visible(True)
-
     drones[drone_id].set_offsets([[x, y]])
     drone_labels[drone_id].set_position((x + 100, y + 100))
-    
+
+
+    # Hyperbolas
+    if show_hyperbolas:
+        # convert nodes to meters
+        nodes_m = gpd.GeoSeries(
+            [Point(lon, lat) for lon, lat in nodes],
+            crs="EPSG:4326",
+        ).to_crs(epsg=3857)
+
+        nodes_xy = np.array([(p.x, p.y) for p in nodes_m])
+
+        # clear old hyperbolas
+        for art in drones[drone_id]._hyperbolas:
+            try:
+                art.remove()
+            except Exception:
+                pass
+        drones[drone_id]._hyperbolas.clear()
+
+        sigma_t = 5e-11
+        pairs_dt = generate_pairwise_tdoa(
+            nodes_xy,
+            np.array([x, y]),
+            pairs=all_pairs(nodes_xy.shape[0]),
+            c=3e8,
+            sigma_t=sigma_t,
+            seed=0,
+        )
+
+        for (i, j, dt) in pairs_dt:
+            try:
+                cs = plot_hyperbola_2d(
+                    ax,
+                    nodes_xy[i],
+                    nodes_xy[j],
+                    dt,
+                    c=3e8,
+                    xlim=(xmin, xmax),
+                    ylim=(ymin, ymax),
+                    grid=300,
+                    alpha=0.15,
+                )
+                if hasattr(cs, "collections"):
+                    drones[drone_id]._hyperbolas.extend(cs.collections)
+            except Exception:
+                pass
+
     fig.canvas.draw_idle()
     fig.canvas.flush_events()
-
-# (lon, lat) around Sumy (approx square)
-nodes = np.array([
-    [34.7680, 50.9430],  # N0
-    [34.8380, 50.9360],  # N1
-    [34.8250, 50.8980],  # N2
-    [34.7750, 50.9060],  # N3
-])
 
 
 def simulate_drone_updates():
@@ -174,14 +213,26 @@ def simulate_drone_updates():
         lat_b = center_lat + 0.06 * np.sin(t + 1.5)
         lon_b = center_lon + 0.10 * np.cos(t + 1.5)
 
+        # Drone C
+        lat_c = center_lat + 0.006 * np.sin(t + 1.5)
+        lon_c = center_lon + 0.010 * np.cos(t + 1.5)
+
         plot_drone((lat_a, lon_a), drone_id="A")
-        plot_drone((lat_b, lon_b), drone_id="B")
+        #plot_drone((lat_b, lon_b), drone_id="B")
+        plot_drone((lat_c, lon_c), drone_id="C")
 
         plt.pause(0.05) 
         t += 0.08
 
 
 if __name__ == "__main__":
-    plot_map(nodes)
+    # (lon, lat) around Sumy
+    receivers = np.array([
+        [34.7680, 50.9430],  # N0
+        [34.8380, 50.9360],  # N1
+        [34.8250, 50.8980],  # N2
+        [34.7750, 50.9060],  # N3
+    ])
+    plot_map(receivers)
 
     simulate_drone_updates()
